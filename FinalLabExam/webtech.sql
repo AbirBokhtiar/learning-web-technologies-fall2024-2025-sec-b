8ec4ad771e39da0a87dfd41bb0b870b87e71e80f
AbirBokhtiar <abirbokhtiar107@gmail.com>
2025-01-12 13:32:07 +0600

    Add files via upload

---
+++ FinalLabExam/webtech.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 12, 2025 at 08:31 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webtech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `employee_name` varchar(128) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`employee_name`, `contact_no`, `username`, `password`) VALUES
+('abir', 1813482058, 'abir22', 'abir11'),
+('bokhtiar', 0, '', '1234'),
+('', 0, 'bokhtiar2', '1234'),
+('abir21', 171147302, 'bokhtiar2', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int(128) NOT NULL,
+  `transaction_id` int(11) NOT NULL DEFAULT 101,
+  `amount` float NOT NULL,
+  `city` varchar(128) NOT NULL,
+  `date` date NOT NULL,
+  `status` varchar(128) NOT NULL,
+  `station_company` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `transaction_id`, `amount`, `city`, `date`, `status`, `station_company`) VALUES
+(1, 1, 120, 'Dhaka', '2025-01-01', 'Done', 'Elecro Company'),
+(2, 2, 110, 'Chittagong', '2025-01-03', 'Pending', 'Charzer'),
+(3, 101, 160, 'Rangpur', '2025-01-05', 'Done', 'VehicleCharge');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(128) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `transaction_id` int(128) NOT NULL DEFAULT 101
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `transaction_id`) VALUES
+(1, 'ab', '11', 'ab@gmail.com', 101),
+(2, 'bokhtiar', '123456', 'bokhtiarbooks@gmail.com', 102);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `transaction_id` (`transaction_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `transaction_id_2` (`transaction_id`),
+  ADD KEY `transaction_id` (`transaction_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(128) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
